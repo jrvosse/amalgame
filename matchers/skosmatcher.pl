c91fc00a47a446caa1fdeb0e182ce5255b7f8306
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2010-06-24 14:10:51 +0200

    wip

--- matchers/skosmatcher.pl
+++ matchers/skosmatcher.pl
@@ -6,27 +6,48 @@
 :- use_module(library(semweb/rdf_db)).
 :- use_module('../mapping_io/edoal').
 
-%%	skos_find_correspondences(+C, +S, +Options, -Result) is det.
+%%	skos_find_candidates(+C, +S, +Options, -Result) is det.
 %
 %	Return all correspondences candidates for for SKOS concept C
 %	from SKOS scheme S. Result is an RDF graph in (extended) EDOAL format
+%
+%	Options include:
+%	- labels_must_match(true):
+%           Find candidates with match labels (cheap)
 
 skos_find_candidates(SourceConcept, TargetConceptScheme, Options, Correspondences):-
-	ground(SourceConcept),
-	ground(TargetConceptScheme),
-	ground(Options),
 	findall(Correspondence,
-		is_correspondence(SourceConcept, TargetConceptScheme, Options, Correspondence),
+		candidate(SourceConcept, TargetConceptScheme, Options, Correspondence),
 		Correspondences
 	       ).
 
-is_correspondence(SourceConcept, TargetConceptScheme, Options, Correspondence) :-
+%%	candidate(+C, +S, +Options, -Correspondence) is det.
+%
+%	Evaluates to true if Correspondence describes an EDOAL cell
+%	with a match from SourceConcept to a target concept from
+%	TargetConceptScheme
+%
+%       Correspondence has a low confidence level,
+%       to indicate that further checking will be needed to
+%       evaluate the quality of this candidate.
+%
+%       The method will list which (pref/alt) labels properties
+%       where used for the match.
+%
+%       For descriptions of possible Options, see above.
+
+candidate(SourceConcept, TargetConceptScheme, Options, Correspondence) :-
+	ground(SourceConcept),
+	ground(TargetConceptScheme),
+	ground(Options),
 	option(labels_must_match(LabelMatch), Options, true),
 	LabelMatch = true,
-	rdf_has(SourceConcept, rdfs:label, literal(Label)),
-	forall((rdf_has(TargetConcept, rdfs:label, literal(Label)),
-		rdf_has(TargetConcept, skos:inScheme, TargetConceptScheme)
-	       ),
-	       create_cell(SourceConcept, TargetConcept, Options, Correspondence)
-	      ),
-	true.
+	rdf_has(SourceConcept, rdfs:label, literal(Label), RealLabel1Predicate),
+	rdf_has(TargetConcept, rdfs:label, literal(Label), RealLabel2Predicate),
+	rdf_has(TargetConcept, skos:inScheme, TargetConceptScheme),
+	format(atom(Method), '~p-~p', [RealLabel1Predicate, RealLabel2Predicate]),
+	CellOptions = [measure(0.001), % Only label match, this is just a candidate
+		       method(Method)
+		       |Options
+		      ],
+	create_cell(SourceConcept, TargetConcept, CellOptions, Correspondence).
