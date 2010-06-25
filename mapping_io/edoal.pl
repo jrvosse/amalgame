0d471e9793357e86da91bc145a406c0f2febc61d
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2010-06-25 12:31:50 +0200

    wip

--- mapping_io/edoal.pl
+++ mapping_io/edoal.pl
@@ -58,32 +58,40 @@ assert_alignment(URI, Options) :-
 %
 %	Creates an EDOAL map cel defining a mapping
 %       between Entity 1 and 2.
-%       OptionList must contain the alignment(A) this cell belongs to.
-%	Optionally, it can also define:
-%	- graph(G) the named graph of the cell (G defaults to 'align')
-%	- measure(M) the confidence level (M defaults to 1.0).
+%	Options include:
+%	- measure(M) the confidence level (M defaults to 0.00001).
 %	- relation(R) the type of relation (R defaults to '=')
 %	- method(M) method used if different from method set for
-%	  aligmnent A (default: none)
+%	- aligmnent A (default: none)
 
 create_cell(C1, C2, Options, TripleList) :-
-        option(alignment(A), Options),
-	option(measure(M),   Options, 1.0),
+	option(measure(M),   Options, 0.00001),
 	option(relation(R),  Options, '='),
         rdf_bnode(Cell),
-        L = [
-	     rdf(A, align:map, Cell),
+        L0 = [
 	     rdf(Cell, rdf:type, align:'Cell'),
 	     rdf(Cell, align:entity1, C1),
 	     rdf(Cell, align:entity2, C2),
 	     rdf(Cell, align:measure, literal(M)),
 	     rdf(Cell, align:relation, literal(R))
 	    ],
+	(   option(alignment(A), Options)
+	->  append(L0, [rdf(A, align:map, Cell)], L1)
+	;   L0 = L1
+	),
 	(   option(method(Method), Options)
-	->  append(L, [rdf(Cell, align:method, literal(Method))], TripleList)
-	;   TripleList = L
+	->  append(L1, [rdf(Cell, align:method, literal(Method))], TripleList)
+	;   TripleList = L1
 	).
 
+%%	assert_cell(+C1,+C2,+OptionList) is det.
+%
+%	Creates and asserts a possibly empty
+%	correspondence between C1 and C2.
+%
+%       OptionList must contain the -
+%	alignment(A) this cell belongs to. It may also define: -
+%	graph(G), named graph to assert to, defaults to 'align'
 
 assert_cell(C1, C2, Options) :-
         option(graph(Graph), Options, align),
--- rankers/skosranker.pl
+++ rankers/skosranker.pl
@@ -7,31 +7,42 @@
 :- use_module(library(semweb/rdf_db)).
 :- use_module('../mapping_io/edoal').
 
-rank_candidates(Concept, Candidates, Options) :-
+rank_candidates(_Concept, Candidates, Options) :-
 	findall(Justifications,
 		(   member(Cand, Candidates),
-		    justify(Concept, Cand, Options, Justifications)
+		    justify(Cand, Options, Justifications)
 		),
 	       JustList),
 	rank_just_list(JustList, _Results).
 
 rank_just_list(JustList, JustList).
 
-justify(C1, C2, Options, Justifications) :-
+justify(Cand, Options, Justifications) :-
 	findall(Justification,
-		justification(C1, C2, Options, Justification),
+		justification(Cand, Options, Justification),
 		Justifications).
 
 :- rdf_meta
    meta_property(+,r).
 
-match_property(def, skos:definition).
+match_property(propmatch:def,  P):- rdf_equal(skos:definition, P).
+match_property(propmatch:pref, P):- rdf_equal(skos:prefLabel, P).
+match_property(propmatch:alt,  P):- rdf_equal(skos:altLabel, P).
 
-justification(C1, C2, _Options, MatchType) :-
+justification(Cand, _Options, MatchType) :-
 	match_property(MatchType, Property),
+	memberchk(rdf(_,align:entity1, C1), Cand),
+	memberchk(rdf(_,align:entity2, C2), Cand),
 	rdf_has(C1, Property, Value),
 	rdf_has(C2, Property, Value).
 
+justification(Cand, _Options, MatchType) :-
+	match_property(MatchType, Property),
+	memberchk(rdf(_,align:entity1, C1), Cand),
+	memberchk(rdf(_,align:entity2, C2), Cand),
+	rdf_has(C1, Property, literal(lang(Lang1, Value))),
+	rdf_has(C2, Property, literal(lang(Lang2, Value))),
+	lang_matches(Lang1, Lang2).
 
 
 
