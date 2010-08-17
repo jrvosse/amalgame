490b74f6bbdd78a64c2d95fb2e39f79d4b066dc8
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2010-08-17 10:52:25 +0200

    work in progress

--- compare/compare.pl
+++ compare/compare.pl
@@ -21,8 +21,47 @@ It assumes matchers assert mappings in different name graphs.
 :- use_module('../namespaces').
 
 
-:- http_handler(amalgame(list_alignments),    list_alignments,     []).
-:- http_handler(amalgame(find_overlap),       find_overlap,        []).
+:- http_handler(amalgame(list_alignments),    http_list_alignments,     []).
+:- http_handler(amalgame(find_overlap),       http_find_overlap,        []).
+
+%%	http_list_alignments(+Request) is det.
+%
+%	HTTP handler returning list of all alignments in HTML.
+
+http_list_alignments(_Request) :-
+	clear_nicknames,
+	reply_html_page(cliopatria(default),
+			title('Alignments'),
+			[ h4('Alignments in the RDF store'),
+			  \show_alignments
+			]).
+
+
+%%	http_find_overlap(+Request) is det.
+%
+%	HTTP handler generating a page with mapping overlap statistics.
+
+http_find_overlap(_Request) :-
+	find_overlap(TotalNr, CountList),
+	reply_html_page(cliopatria(default),
+			[
+			     title('Alignment overlap'),
+			     style([type('text/css')],
+				   ['#aligntable { padding: .3%; border: solid grey;  float: left }',
+				    '#nicktable  { padding: .3%; border: dashed grey; float: left; margin-left: 5% }',
+				    '#totals td  { border-top: solid grey; font-weight: bold }'
+				   ])
+			],
+			[
+			 h4('Alignment overlap'),
+			 table([id(aligntable)],
+			       [
+				\show_countlist(CountList),
+				tr([id(totals)],[td(TotalNr), td('Total')])
+				]
+			      ),
+			 table([id(nicktable)],\show_alignments)
+			]).
 
 %%	has_map(+E1, +E2, -Format, -Graph) is non_det.
 %
@@ -35,7 +74,10 @@ It assumes matchers assert mappings in different name graphs.
 %	@see EDOAL: http://alignapi.gforge.inria.fr/edoal.html
 
 has_map([E1, E2], edoal, Graph) :-
+	% FIXME: workaround rdf/4 index bug
+	rdf(Cell, align:entity1, E1),
 	rdf(Cell, align:entity1, E1, Graph),
+	rdf(Cell, align:entity2, E2),
 	rdf(Cell, align:entity2, E2, Graph).
 
 has_map([E1, E2], skos, Graph) :-
@@ -68,88 +110,124 @@ find_graphs(Map, Graphs) :-
 
 count_alignments(Format, Graph, Count) :-
 	findall(Map, has_map(Map, Format, Graph), Graphs),
-	length(Graphs, Count).
+	length(Graphs, Count),!.
 
 count_alignments(_,_,-1).
 
-find_overlap(TotalNr, ResultAssoc) :-
+find_overlap(TotalNr, ResultsSorted) :-
 	findall(Map, map_iterator(Map), AllMaps),
 	length(AllMaps, TotalNr),
-	debug(compare, 'Found ~w mappings', [TotalNr]),
-	empty_assoc(EmptyAssoc),
-	find_overlap(AllMaps, EmptyAssoc, ResultAssoc).
+	find_overlaps(AllMaps, [], Overlaps),
+	count_overlaps(Overlaps, [], Results),
+	sort(Results, ResultsSorted).
 
-spyme.
-find_overlap([], Assoc, Assoc) :- spyme.
-
-find_overlap([Map|Tail], In, Out) :-
+find_overlaps([], Doubles, Uniques) :- sort(Doubles, Uniques).
+find_overlaps([Map|Tail], Accum, Out) :-
 	find_graphs(Map, Graphs),
-	(   get_assoc(Graphs, In, OldMappings)
-	->  append(OldMappings, [Map], NewMappings)
-	;   NewMappings = [Map]
-	),
-	put_assoc(Graphs, In, NewMappings, NewAssoc),
-	find_overlap(Tail, NewAssoc, Out).
+	find_overlaps(Tail, [Graphs:Map|Accum], Out).
 
-count_mappings(Assoc, Key, Key:Count) :-
-	get_assoc(Key, Assoc, MapList),
-	length(MapList, Count).
-
-list_alignments(_Request) :-
-	findall(Format:Graph, has_map(_, Format,Graph:_), Graphs),
-	sort(Graphs, SortedGraphs),
-	reply_html_page(cliopatria(default),
-			title('Alignments'),
-			[ h4('Alignments in the RDF store'),
-			  table([tr([th(format),
-				     th('# maps'),
-				     th('named graph')
-				    ]),
-				 \show_alignments(SortedGraphs)])
-			]).
+count_overlaps([], Results, Results).
+count_overlaps([Graphs:_|T], Accum, Results) :-
+	member(_:Graphs, Accum),!,
+	count_overlaps(T, Accum, Results).
+count_overlaps([Graphs:_|Tail], Accum, Results) :-
+	findall(Graphs, member(Graphs:_, Tail), Members),
+	length(Members, NrMembers),
+	Count is NrMembers + 1,
+	debug(compare, '~w: ~w', [Count, Graphs]),
+	count_overlaps(Tail, [Count:Graphs|Accum], Results).
 
 
-show_alignments([]) --> !.
-show_alignments([Format:Graph|Tail]) -->
+clear_nicknames :-
+% work around bug in rdf/4
+	% rdf_retractall(_, amalgame:nickname, _, amalgame_nicknames).
+	rdf_retractall(_, amalgame:nickname, _).
+has_nickname(Graph,Nick) :-
+	% work around bug in rdf/4
+	% rdf(Graph, amalgame:nickname, literal(Nick), amalgame_nicknames).
+	rdf(Graph, amalgame:nickname, literal(Nick)).
+nickname(Graph, Nick) :-
+	has_nickname(Graph,Nick), !.
+nickname(Graph, Nick) :-
+	coin_nickname(Graph, Nick),
+	rdf_assert(Graph, amalgame:nickname, literal(Nick), amalgame_nicknames).
+coin_nickname(_Graph, Nick) :-
+	char_type(Nick, alpha),
+	\+ has_nickname(_, Nick).
+
+show_graph(Graph, Options) -->
 	{
-	 count_alignments(Format, Graph, Count)
+	 member(nick(true), Options),!,
+	 nickname(Graph, Nick),
+	 http_link_to_id(list_graph, [graph(Graph)], VLink)
 	},
-	html(tr([td(Format),
-		 td(Count),
-		 td(\show_graph(Graph))
-		])),
-	show_alignments(Tail).
+	html(a([href(VLink),title(Graph)],[Nick, ' '])).
 
-
-show_graph(Graph) -->
+show_graph(Graph, _Options) -->
 	{
 	 http_link_to_id(list_graph, [graph(Graph)], VLink)
 	},
 	html(a([href(VLink)],\turtle_label(Graph))).
 
-%%	find_overlap(+Request) is det.
-%
-%	Find overlap statistics for a predefined mapping.
-
-find_overlap(_Request) :-
-	find_overlap(TotalNr, ResultAssoc),
-	assoc_to_keys(ResultAssoc, KeyList),
-	maplist(count_mappings(ResultAssoc), KeyList, CountList),
-	reply_html_page(cliopatria(default),
-			title('Alignment overlap'),
-			[
-			 h4('Alignment overlap'),
-			 p([TotalNr]),
-			 table(\show_countlist(CountList))
-			]).
-
 show_countlist([]) --> !.
-show_countlist([L:C|T]) -->
-	html(tr([td(C), td(\show_graphs(L))])),
+show_countlist([C:L|T]) -->
+	html(tr([td(C), td(\show_graphs(L, [nick(true)]))])),
 	show_countlist(T).
 
 
-show_graphs([]) --> !.
-show_graphs([H|T]) -->
-	show_graph(H),
-	show_graphs(T).
+show_graphs([],_) --> !.
+show_graphs([H|T], Options) -->
+	show_graph(H, Options),
+	show_graphs(T, Options).
+
+show_nicknames -->
+	{
+	 findall(Nick:Graph, has_nickname(Graph,Nick), AllNicks),
+	 sort(AllNicks, Nicks)
+	},
+	html(tr([th('Legend'), th('Graph')])),
+	show_nicknames(Nicks).
+
+show_nicknames([]) --> !.
+show_nicknames([Nick:Graph|Tail]) -->
+	html(tr([td(Nick), td(\show_graph(Graph, [nick(false)]))])),
+	show_nicknames(Tail).
+
+show_alignments -->
+	{
+	 findall(Format:Graph,
+		 has_map(_, Format,Graph:_),
+		 Graphs),
+	 sort(Graphs, UniqueGraphs),
+	 findall(Count:Format:Graph,
+		 (   member(Format:Graph, UniqueGraphs),
+		     count_alignments(Format, Graph, Count)
+		 ),
+		 CountedGraphs),
+	 sort(CountedGraphs, SortedGraphs)
+	},
+	html(table([tr([
+			th('abbrev'),
+			th(format),
+			th('# maps'),
+			th('named graph')
+		       ]),
+		    \show_alignments(SortedGraphs)
+		   ])).
+
+show_alignments([]) --> !.
+show_alignments([Count:Format:Graph|Tail]) -->
+	html(tr([
+		 td(\show_graph(Graph, [nick(true)])),
+		 td(Format),
+		 td(Count),
+		 td(\show_graph(Graph, [nick(false)]))
+		])),
+	show_alignments(Tail).
+
+
+
+
+
+
+
