a9c056d4169e5cab9b284bd43e14a0694de04ebf
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2010-09-23 11:09:55 +0200

    show amalgame version on the page footer

--- skin.pl
+++ skin.pl
@@ -51,17 +51,23 @@ logo -->
 	    ).
 
 cliopatria:server_address -->
-	{ git_component_property('ClioPatria', version(CP_Version)),
-	  Home = 'http://www.swi-prolog.org/web/ClioPatria.html'
+	{
+	  git_component_property('ClioPatria', version(CP_Version)),
+	  git_component_property('ClioPatria', home_url(CP_Home)),
+	  git_component_property('amalgame', version(AG_Version)),
+	  git_component_property('amalgame', home_url(AG_Home))
+
 	},
 	html_requires(css('cliopatria.css')),
 	html([ 	\logo,
 		address([id(amalgame)],
-			a([href('http://eculture.cs.vu.nl/git/econnect/amalgame.git')],
-			  'Amalgame')),
+			a(href(AG_Home),
+			  'Amalgame ~w'-[AG_Version])
+		       ),
 	       address(class(cliopatria),
 		       [
-			 a(href(Home), 'ClioPatria ~w'-[CP_Version]),
+			 a(href(CP_Home),
+			   'ClioPatria ~w'-[CP_Version]),
 			 \current_page_doc_link
 		       ])
 	     ]).
