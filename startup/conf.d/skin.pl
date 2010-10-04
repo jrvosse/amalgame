399c65b33c8352d5d31b6cdadc38092d6208f946
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2010-10-04 17:54:13 +0200

    first sketch for an amalgame home page

--- startup/conf.d/skin.pl
+++ startup/conf.d/skin.pl
@@ -1,5 +1,6 @@
 :- module(ag_skin, []).
 
+:- use_module(library(http/http_dispatch)).
 :- use_module(library(http/html_write)).
 :- use_module(library(http/html_head)).
 :- use_module(library(http/http_path)).
@@ -13,7 +14,7 @@
 user:file_search_path(icons, amalgame('web/img')).
 user:file_search_path(css,   amalgame('web/css')).
 
-:- set_setting(graphviz:format, svg).
+:- set_setting_default(graphviz:format, svg).
 
 :- html_resource(css('amalgame.css'),
 		 [requires([
@@ -34,23 +35,6 @@ cliopatria:page_body(Body) -->
 			)
 		  ])).
 
-
-logo -->
-	{
-	 http_absolute_location(icons('econnect-banner.jpg'), LogoImg, [])
-	},
-	html(a([class(logo),
-		id(econnectlink),
-		target(new),
-		href('http://www.europeanaconnect.eu/'),
-		style('float: left; margin-right: 1%;')
-	       ],
-	       img([src(LogoImg),
-		    style('border-style: none;'),
-		    alt('EuropeanaConnect project logo')
-		   ],[])
-	      )
-	    ).
 
 cliopatria:server_address -->
 	{
@@ -74,3 +58,40 @@ cliopatria:server_address -->
 		       ])
 	     ]).
 
+user:body(amalgame(search), Body) -->
+	{
+	 http_link_to_id(http_list_skos_vocs, [], BackOfficeLink)
+	},
+	html_requires(css('amalgame.css')),
+	html(body(class('yui-skin-sam ag_search'),
+		  [
+		    div(class(ag_search),
+			[
+			 \simple_search_form,
+			 div(id(content), Body)
+			]),
+			br(clear(all)),
+			div([id(address)],
+			 \(cliopatria:server_address)
+			),
+		        div([class(backoffice)],
+			    [a(href(BackOfficeLink), 'back office')
+			    ])
+		  ])).
+
+logo -->
+	{
+	 http_absolute_location(icons('econnect-banner.jpg'), LogoImg, [])
+	},
+	html(a([class(logo),
+		id(econnectlink),
+		target(new),
+		href('http://www.europeanaconnect.eu/'),
+		style('float: left; margin-right: 1%;')
+	       ],
+	       img([src(LogoImg),
+		    style('border-style: none;'),
+		    alt('EuropeanaConnect project logo')
+		   ],[])
+	      )
+	    ).
