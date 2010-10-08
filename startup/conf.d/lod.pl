24169ecf8d4d3ad4f87382372305347ad90cd381
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2010-10-08 14:39:57 +0200

    lod work in progress

--- startup/conf.d/lod.pl
+++ startup/conf.d/lod.pl
@@ -21,13 +21,6 @@ server and all RDF URIs start with http://www.purl.org/mydata/
 @see cliopatria(api/lod)
 */
 
-%%      cliopatria:lod_description(+URI, -Graph) is det.
-%
-%	Override standard CBD by Symmetric CMD (scbd) expansion.
-
-cliopatria:lod_description(URI, Graph) :-
-	rdf_bounded_description(rdf, scbd, URI, Graph).
-
 :- http_handler('/lod/purl/vocabularies/', lod_api,
                 [ redirected_from('http://purl.org/vocabularies/'),
                   prefix
@@ -44,4 +37,44 @@ cliopatria:lod_description(URI, Graph) :-
                 ]).
 
 
+%%      cliopatria:lod_description(+URI, -Graph) is det.
+%
+%	Override standard CBD by Symmetric CMD (scbd) expansion.
 
+cliopatria:lod_description(URI, Graph) :-
+	rdf_bounded_description(rdf, scbd, URI, Graph).
+
+cliopatria:redirect_uri(html, URI, SeeOther) :-
+	rdf(URI,
+	    'http://www.w3.org/2006/03/wn/wn20/schema/senseLabel',
+	    literal(lang(_, SL))),
+        sub_atom(URI,_,1,0,IndexAtom),
+        atom_number(IndexAtom, IndexOffByOne),
+        Index is IndexOffByOne - 1,
+        Base='http://wordnetweb.princeton.edu/perl/webwn?s=~w&i=~w',
+        sformat(SeeOther, Base, [SL,Index]).
+
+% Redirect HTML requests to resource's landing page if defined
+cliopatria:redirect_uri(html, URI, SeeOther) :-
+	rdf(URI, 'http://www.europeana.eu/schemas/edm/landingPage', SeeOther),!.
+cliopatria:redirect_uri(html, URI, SeeOther) :-
+	rdf(URI, 'http://purl.org/collections/bibliopolis/landingPage', SeeOther),!.
+
+% Redirect HTML requests for wordnet to Princeton
+cliopatria:redirect_uri(html, URI, SeeOther) :-
+	rdf(URI,
+	    'http://www.w3.org/2006/03/wn/wn20/schema/senseLabel',
+	    literal(lang(_, SL))),
+        sub_atom(URI,_,1,0,IndexAtom),
+        atom_number(IndexAtom, IndexOffByOne),
+        Index is IndexOffByOne - 1,
+        Base='http://wordnetweb.princeton.edu/perl/webwn?s=~w&i=~w',
+        sformat(SeeOther, Base, [SL,Index]),
+	!.
+
+% Redirect HTML requests for AAT to Getty
+cliopatria:redirect_uri(html, URI, SeeOther) :-
+        rdf(URI, skos:inScheme, 'http://purl.org/vocabularies/getty/aat'),
+        rdf(URI, vp:id, literal(Id)),
+        Base='http://www.getty.edu/vow/AATFullDisplay?find=&logic=AND&note=&english=N&subjectid=',
+        atom_concat(Base, Id, SeeOther).
