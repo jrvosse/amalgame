c56e0151071c121d106321d3572355e1dd574d5f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2010-11-22 14:04:43 +0100

    More renaming: move amalgame to config-available

--- config-available/020-amalgame.pl
+++ config-available/020-amalgame.pl
@@ -1,6 +1,26 @@
 :- module(conf_amalgame, []).
+:- module(amalgame, []).
+:- use_module(cliopatria(cliopatria)).	% cp_after_load/1.
+:- use_module(library(semweb/rdf_library)).
 
-/** <module> Load Amalgame
+/** <module> Setup amalgame
 */
 
-:- use_module('../amalgame').
+:- multifile http:location/3.
+
+http:location(amalgame, cliopatria(amalgame), []).
+
+ag_load_schemas :-
+	rdf_attach_library(amalgame(rdf)),
+	rdf_load_library('Amalgame').
+
+:- cp_after_load(ag_load_schemas).
+
+:- use_module([ amalgame(namespaces),
+		applications(align_stats),
+		applications(alignment/alignment),
+		applications(vocabularies/vocabularies),
+		applications(concept_finder/concept_finder),
+		applications(equalizer/equalizer),
+		applications(evaluator/evaluator)
+	      ]).
