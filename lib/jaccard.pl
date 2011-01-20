141aaecb407c85fc2cf16b86f16953d88fde71ec
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-01-20 09:12:27 +0100

    replaced tokenize_atom by rdf_tokenize_literal to get rid of stop words etc

--- lib/jaccard.pl
+++ lib/jaccard.pl
@@ -7,14 +7,16 @@
 @see http://en.wikipedia.org/wiki/Jaccard_coefficient
 */
 
+:- use_module(library(semweb/rdf_litindex)).
+
 %%	jaccard_coefficient(+Atom1, +Atom2, -Similarity) is det.
 %
 %	Returns the jaccard similarity coefficient of two atoms,
 %	based on the overlap of their tokens.
 
 jaccard_similarity(S1, S2, Similarity) :-
-	tokenize_atom(S1, T1),
-	tokenize_atom(S2, T2),
+	rdf_tokenize_literal(S1, T1),
+	rdf_tokenize_literal(S2, T2),
 	sort(T1, Sorted1),
 	sort(T2, Sorted2),
 	ord_union(Sorted1, Sorted2, Union),
