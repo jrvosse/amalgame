709149ea5ff7f3923ee0a3f021c1eea9fa08ad24
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-03-16 15:34:44 +0100

    source/target disamb option added

--- lib/amalgame/partition/best_numeric.pl
+++ lib/amalgame/partition/best_numeric.pl
@@ -2,6 +2,7 @@
 
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(semweb/rdfs)).
+:- use_module(library(amalgame/alignment_graph)).
 
 :- public partition/3.
 :- multifile amalgame:component/2.
@@ -18,16 +19,24 @@ amalgame:component(partition, best_numeric(alignment_graph, [selected(alignment_
 %	ones.
 %       Input is a sorted list of alignment terms.
 
-partition(AlignmentGraph, ListOfGraphs, _Options) :-
+partition(AlignmentGraph, ListOfGraphs, Options) :-
 	ListOfGraphs = [selected(S),
 			discarded(D),
 			undecided(U)
 		      ],
-	partition_(AlignmentGraph, S, D, U).
+	option(disamb(SourceOrTarget), Options, source),
+	(   SourceOrTarget = target
+	->  predsort(compare_align(sourceplus), AlignmentGraph, SortedAlignmentGraph)
+	;   predsort(compare_align(targetplus), AlignmentGraph, SortedAlignmentGraph)
+	),
+	partition_(SourceOrTarget, SortedAlignmentGraph, S, D, U).
 
-partition_([], [], [], []).
-partition_([align(S,T,P)|As], Sel, Dis, Und) :-
-	same_source(As, S, Same, Rest),
+partition_(_, [], [], [], []) :- !.
+partition_(SourceOrTarget, [align(S,T,P)|As], Sel, Dis, Und) :-
+	(   SourceOrTarget = target
+	->  same_source(As, S, Same, Rest)
+	;   same_target(As, T, Same, Rest)
+	),
 	(   best_numeric([align(S,T,P)|Same], Selected, Discarded)
 	->  Sel = [Selected|SelRest],
 	    append(Discarded, DisRest, Dis),
@@ -36,21 +45,25 @@ partition_([align(S,T,P)|As], Sel, Dis, Und) :-
 	    Sel = SelRest,
 	    Dis = DisRest
 	),
-	partition_(Rest, SelRest, DisRest, UndRest).
+	partition_(SourceOrTarget, Rest, SelRest, DisRest, UndRest).
 
-same_source([align(S,T,P)|As], S, [align(S,T,P)|Same], Rest) :-
-	!,
-	same_source(As, S, Same, Rest).
+same_source([align(S,T,P)|As], S, [align(S,T,P)|Same], Rest) :-	!,  same_source(As, S, Same, Rest).
 same_source(As, _S, [], As).
 
+same_target([align(S,T,P)|As], T, [align(S,T,P)|Same], Rest) :-	!,  same_target(As, T, Same, Rest).
+same_target(As, _S, [], As).
 
-best_numeric(As, Selected, [A|T]) :-
+best_numeric(As, Selected, Discarded) :-
 	group_match(As, Counts0),
 	!,
-	sort(Counts0, Counts),
-	reverse(Counts, [N-Selected,N1-A|T0]),
-	N > N1,
-	pairs_values(T0, T).
+	(   Counts0 = [_N-Selected]
+	->  Discarded = []
+	;   sort(Counts0, Counts),
+	    reverse(Counts, [N-Selected,N1-A|T0]),
+	    N > N1,
+	    pairs_values(T0, T),
+	    Discarded = [A|T]
+	).
 
 group_match([], []).
 group_match([Align|As], [Match-Align|Ts]) :-
