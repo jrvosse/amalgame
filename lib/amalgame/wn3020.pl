98a7d2642c1e019aee3f6601f640b64c92f3f8f0
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-04-04 09:53:54 +0200

    wip

--- lib/amalgame/wn3020.pl
+++ lib/amalgame/wn3020.pl
@@ -164,29 +164,30 @@ myalign(Type, SourceVoc, TargetVoc) :-
 
 	% Step 1a: align by exact label match on skos:definition,  split off ambiguous and count results
 	% Ambiguous results will be further processed in step 1b, non matching concepts in step 2.
-	align(SourceVoc, TargetVoc, source_candidate, exact_label_match, target_candidate, GlossMatch, OptionsDef, A1a),
-	materialize_alignment_graph(GlossMatch, [graph(rawgloss)]), % fail,
+	align(SourceVoc, TargetVoc, source_candidate, exact_label_match, target_candidate, GlossMatch, OptionsDef, _A1a),
+	% materialize_alignment_graph(GlossMatch, [graph(rawgloss)]), % fail,
 	arity_select:selecter(GlossMatch, UnambiguousGloss, AmbiguousGloss, _, []),
 	debug_partition('Gloss match',  [ambiguous(AmbiguousGloss),unambiguous(UnambiguousGloss)]),
-	materialize_alignment_graph(AmbiguousGloss, [graph(glossNM)]), % fail,
+	% materialize_alignment_graph(AmbiguousGloss, [graph(glossNM)]), % fail,
 
 	% Step 1b: align ambiguous gloss matches by exact label match on skos:altLabel,
 	% split off ambiguous and count results.
 	% We assume ambiguous results here are concepts that are splitted or merged in the two versions
 	% and thus OK
-	align(AmbiguousGloss, _, alignment_element, exact_label_match, _, GlossLabelMatch, OptionsLabel, A1b),
-	materialize_alignment_graph(GlossLabelMatch, [graph(rawglosslabel)]),
+	align(AmbiguousGloss, _, alignment_element, exact_label_match, _, GlossLabelMatch, OptionsLabel, _A1b),
+	% materialize_alignment_graph(GlossLabelMatch, [graph(rawglosslabel)]),
 	arity_select:selecter(GlossLabelMatch, UnambiguousGlossLabel, AmbiguousGlossLabel, _, []),
-	materialize_alignment_graph(UnambiguousGlossLabel, [graph(glosslabel11)]),
-	materialize_alignment_graph(AmbiguousGlossLabel, [graph(glosslabelNM)]),
+	% materialize_alignment_graph(UnambiguousGlossLabel, [graph(glosslabel11)]),
+	% materialize_alignment_graph(AmbiguousGlossLabel, [graph(glosslabelNM)]),
 
 	debug_partition('Ambiguous gloss + label', [ambiguous(AmbiguousGlossLabel),unambiguous(UnambiguousGlossLabel)]),
 	merge_graphs([UnambiguousGloss, UnambiguousGlossLabel, AmbiguousGlossLabel], GoodGlossLabelMatches),
 
 	% Step2a: exact label match on concepts with non-matching glosses
 	% align_exclude:source_select(SourceVoc, Source_rest, [exclude(GoodGlossLabelMatches)]),
-	voc_exclude:concept_selecter(SourceVoc, TargetVoc, Source_rest, _Target_rest, [exclude(GoodGlossLabelMatches)]),
-	align(Source_rest, TargetVoc, source_candidate, exact_label_match, target_candidate, LabelMatch0, OptionsLabel, _A2a),
+	gtrace,
+	voc_exclude:concept_selecter(SourceVoc, Source_rest, [exclude_sources(GoodGlossLabelMatches)]),
+	align(Source_rest, TargetVoc, alignment_element, exact_label_match, target_candidate, LabelMatch0, OptionsLabel, _A2a),
 	voc_exclude:concept_selecter(LabelMatch0, LabelMatch, [exclude_targets(GoodGlossLabelMatches)]),
 	arity_select:selecter(LabelMatch, UnambiguousLabel, AmbiguousLabel, _, []),
 
@@ -210,9 +211,9 @@ myalign(Type, SourceVoc, TargetVoc) :-
 	rdf_equal(amalgame:untyped,Type),
 	% Step 3: Materialize the good stuff found so far, so we can use this to do structural matching later
 	debug(align, '~p: Materializing alignments found so far.', [Type]),
-	graph_name('1a_unambiguous_gloss',   Type, UnambiguousGlossName),
-	graph_name('1b_gloss_unique_label',  Type, GlossLabelName),
-	graph_name('1c_ambiguous_gloss',     Type, AmbiguousGlossName),
+	% graph_name('1a_unambiguous_gloss',   Type, UnambiguousGlossName),
+	% graph_name('1b_gloss_unique_label',  Type, GlossLabelName),
+	% graph_name('1c_ambiguous_gloss',     Type, AmbiguousGlossName),
 	graph_name('2a_nogloss_uniquelabel', Type, NoGlossLabelName),
 	% graph_name('2b_nogloss_mostlabel',   Type, NoGlossMostLabelName),
 	graph_name('2c_best_gloss_label',    Type, BestGlossLabelName),
