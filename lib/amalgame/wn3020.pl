62421d71ee241cb53a92d526aa3844f5e960a990
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-04-06 13:00:13 +0200

    w.i.p. on wn3020 workflow

--- lib/amalgame/wn3020.pl
+++ lib/amalgame/wn3020.pl
@@ -165,32 +165,25 @@ myalign(Type, SourceVoc, TargetVoc) :-
 	% Step 1a: align by exact label match on skos:definition,  split off ambiguous and count results
 	% Ambiguous results will be further processed in step 1b, non matching concepts in step 2.
 	align(SourceVoc, TargetVoc, source_candidate, exact_label_match, target_candidate, GlossMatch, OptionsDef, _A1a),
-	% materialize_alignment_graph(GlossMatch, [graph(rawgloss)]), % fail,
 	arity_select:selecter(GlossMatch, UnambiguousGloss, AmbiguousGloss, _, []),
 	debug_partition('Gloss match',  [ambiguous(AmbiguousGloss),unambiguous(UnambiguousGloss)]),
-	% materialize_alignment_graph(AmbiguousGloss, [graph(glossNM)]), % fail,
 
 	% Step 1b: align ambiguous gloss matches by exact label match on skos:altLabel,
 	% split off ambiguous and count results.
 	% We assume ambiguous results here are concepts that are splitted or merged in the two versions
 	% and thus OK
 	align(AmbiguousGloss, _, alignment_element, exact_label_match, _, GlossLabelMatch, OptionsLabel, _A1b),
-	% materialize_alignment_graph(GlossLabelMatch, [graph(rawglosslabel)]),
 	arity_select:selecter(GlossLabelMatch, UnambiguousGlossLabel, AmbiguousGlossLabel, _, []),
-	% materialize_alignment_graph(UnambiguousGlossLabel, [graph(glosslabel11)]),
-	% materialize_alignment_graph(AmbiguousGlossLabel, [graph(glosslabelNM)]),
 
 	debug_partition('Ambiguous gloss + label', [ambiguous(AmbiguousGlossLabel),unambiguous(UnambiguousGlossLabel)]),
 	merge_graphs([UnambiguousGloss, UnambiguousGlossLabel, AmbiguousGlossLabel], GoodGlossLabelMatches),
 
 	% Step2a: exact label match on concepts with non-matching glosses
 	% align_exclude:source_select(SourceVoc, Source_rest, [exclude(GoodGlossLabelMatches)]),
-	gtrace,
 	voc_exclude:concept_selecter(SourceVoc, Source_rest, [exclude_sources(GoodGlossLabelMatches)]),
 	align(Source_rest, TargetVoc, alignment_element, exact_label_match, target_candidate, LabelMatch0, OptionsLabel, _A2a),
 	voc_exclude:concept_selecter(LabelMatch0, LabelMatch, [exclude_targets(GoodGlossLabelMatches)]),
 	arity_select:selecter(LabelMatch, UnambiguousLabel, AmbiguousLabel, _, []),
-
 
 	% Step 2b: First try disambiguation by counting number of matching sense labels (cheap, counting existing matches)
 	% most_labels_jacco:partition(LabelMatch,
@@ -198,15 +191,14 @@ myalign(Type, SourceVoc, TargetVoc) :-
 	% debug_partition('Exact label, most label', [selected(MostLabels), discarded(Discarded2), undecided(AmbiguousLabel2)]),
 
 	% Step 2c: Then try disambiguation by jaccard similarity on the glosses (more expensive, need to compute jaccard)
-	% align(AmbiguousLabel, TargetVoc, alignment_element, jaccard_match, target_candidate, JaccardMatch, OptionsDef, A2c),
-	align(AmbiguousLabel, TargetVoc, alignment_element, isub_match, target_candidate, JaccardMatch, OptionsDef, _A2c),
-	best_numeric:partition(JaccardMatch, JaccardSPartition, [disamb(source)]),
-	debug_partition(jaccard_gloss_source, JaccardSPartition),
-	memberchk(selected(BestGlossAndLabelS), JaccardSPartition),
-	memberchk(undecided(AmbiLabelS), JaccardSPartition),
-	best_numeric:partition(BestGlossAndLabelS, JaccardTPartition, [disamb(target)]),
-	memberchk(selected(BestGlossAndLabel), JaccardTPartition),
-	memberchk(undecided(AmbiLabelT), JaccardSPartition),
+	align(AmbiguousLabel, TargetVoc, alignment_element, isub_match, target_candidate, NearMatch, OptionsDef, _A2c),
+	best_numeric:partition(NearMatch, NearSPartition, [disamb(source)]),
+	debug_partition(jaccard_gloss_source, NearSPartition),
+	memberchk(selected(BestGlossAndLabelS), NearSPartition),
+	memberchk(undecided(AmbiLabelS), NearSPartition),
+	best_numeric:partition(BestGlossAndLabelS, NearTPartition, [disamb(target)]),
+	memberchk(selected(BestGlossAndLabel), NearTPartition),
+	memberchk(undecided(AmbiLabelT), NearSPartition),
 
 	rdf_equal(amalgame:untyped,Type),
 	% Step 3: Materialize the good stuff found so far, so we can use this to do structural matching later
@@ -214,16 +206,16 @@ myalign(Type, SourceVoc, TargetVoc) :-
 	% graph_name('1a_unambiguous_gloss',   Type, UnambiguousGlossName),
 	% graph_name('1b_gloss_unique_label',  Type, GlossLabelName),
 	% graph_name('1c_ambiguous_gloss',     Type, AmbiguousGlossName),
-	graph_name('2a_nogloss_uniquelabel', Type, NoGlossLabelName),
+	% graph_name('2a_nogloss_uniquelabel', Type, NoGlossLabelName),
 	% graph_name('2b_nogloss_mostlabel',   Type, NoGlossMostLabelName),
-	graph_name('2c_best_gloss_label',    Type, BestGlossLabelName),
+	% graph_name('2c_best_gloss_label',    Type, BestGlossLabelName),
 	% graph_name('xz_ambiguous_label',     Type, AmbiLabelName),
 	% materialize_alignment_graph(UnambiguousGloss,      [graph(UnambiguousGlossName), process(A1a)]),
-	%materialize_alignment_graph(AmbiguousGlossLabel,   [graph(AmbiguousGlossName), process(A1b)]),
+	% materialize_alignment_graph(AmbiguousGlossLabel,   [graph(AmbiguousGlossName), process(A1b)]),
 	% materialize_alignment_graph(UnambiguousGlossLabel, [graph(GlossLabelName), process(A1a)]),
-	materialize_alignment_graph(UnambiguousLabel,      [graph(NoGlossLabelName)]),
+	% materialize_alignment_graph(UnambiguousLabel,      [graph(NoGlossLabelName)]),
 	% materialize_alignment_graph(MostLabels,	           [graph(NoGlossMostLabelName)]),
-	materialize_alignment_graph(BestGlossAndLabel,	   [graph(BestGlossLabelName)]),
+	% materialize_alignment_graph(BestGlossAndLabel,	   [graph(BestGlossLabelName)]),
 	% materialize_alignment_graph(AmbiLabel,             [graph(AmbiLabelName)]),
 
 	% Disambiguate remaining with structural properties
@@ -234,9 +226,9 @@ myalign(Type, SourceVoc, TargetVoc) :-
 	merge_graphs([ToBeAmbiguated, AncMatch, DecMatch, RelMatch], DisambResults),
 	most_methods:partition(DisambResults, MostMethods, []),
 	debug_partition('Most methods: ', MostMethods),
-	memberchk(selected(MostMethodsLabelMatch), MostMethods),
-	graph_name('2d_disamb_label', Type, MostMethodLabelName),
-	materialize_alignment_graph(MostMethodsLabelMatch, [graph(MostMethodLabelName)]),
+	% memberchk(selected(MostMethodsLabelMatch), MostMethods),
+	% graph_name('2d_disamb_label', Type, MostMethodLabelName),
+	% materialize_alignment_graph(MostMethodsLabelMatch, [graph(MostMethodLabelName)]),
 
 	% graph_name(ambiguous_undecided, Type, LeftOversName),
 	% member(undecided(LeftOvers), MostMethods),
