4079098eb772fa0148ab97e31362b12184d2a46a
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-04-22 10:19:32 +0200

    missing use_module

--- lib/amalgame/alignment_graph.pl
+++ lib/amalgame/alignment_graph.pl
@@ -27,6 +27,7 @@
 :- use_module(library(amalgame/matchers/snowball_match)).
 :- use_module(library(amalgame/matchers/exact_label_match)).
 :- use_module(library(amalgame/select/select1_1)).
+:- use_module(library(amalgame/source/voc_exclude)).
 
 :- dynamic
 	map_cache/2.
