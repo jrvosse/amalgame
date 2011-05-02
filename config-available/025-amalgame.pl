ea68d536c4239d035e167cad2e25a2b6876b5d6e
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-05-02 18:30:29 +0200

    do not load old code

--- config-available/025-amalgame.pl
+++ config-available/025-amalgame.pl
@@ -12,10 +12,10 @@ http:location(amalgame, cliopatria(amalgame), []).
 :- rdf_attach_library(amalgame(rdf)).
 :- rdf_load_library(amalgame).
 
-:- use_module([ applications(align_stats),
-		applications(alignment/alignment),
-		applications(vocabularies/vocabularies),
-		applications(concept_finder/concept_finder),
-		applications(equalizer/eq),
-		applications(evaluator/evaluator)
+:- use_module([ % applications(align_stats),
+		% applications(alignment/alignment),
+		% applications(vocabularies/vocabularies),
+		% applications(concept_finder/concept_finder),
+		% applications(evaluator/evaluator),
+		applications(equalizer/eq)
 	      ]).
