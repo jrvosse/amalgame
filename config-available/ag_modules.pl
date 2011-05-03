be3540487434c0a66a5d1dd34eb90cc4a706e05a
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-05-03 12:27:49 +0200

    added arity select module

--- config-available/ag_modules.pl
+++ config-available/ag_modules.pl
@@ -4,4 +4,5 @@ user:file_search_path(ag_modules, library(ag_modules)).
 
 :- use_module(ag_modules(exact_label_match)).
 :- use_module(ag_modules(snowball_match)).
-:- use_module(ag_modules(select1_1)).
+:- use_module(ag_modules(select1_1)).  % I think we should deprecate this, is really select N-1
+:- use_module(ag_modules(arity_select)).
---
+++ lib/ag_modules/arity_select.pl
@@ -0,0 +1,35 @@
+:- module(arity_select,[]).
+
+:- use_module(library(semweb/rdf_db)).
+:- use_module(library(amalgame/map)).
+:- use_module(library(amalgame/partition/source_ambiguity)).
+:- use_module(library(amalgame/partition/target_ambiguity)).
+
+:- public amalgame_module/1.
+:- public selecter/5.
+:- public input/1.
+
+amalgame_module(amalgame:'Arity_select').
+input(amalgame:'Mapping').
+
+
+%%	selecter(+Source, -Selected, -Discarded, -Undecided, +Options)
+%
+
+selecter(AlignmentGraph, Sel, Dis, [], Options) :-
+	predsort(ag_map:compare_align(sourceplus), AlignmentGraph, SourceSorted),
+	predsort(ag_map:compare_align(targetplus), AlignmentGraph, TargetSorted),
+
+	target_ambiguity:partition(SourceSorted, Lsource, Options),
+	source_ambiguity:partition(TargetSorted, Ltarget, Options),
+
+	memberchk(unambiguous(Source1), Lsource),
+	memberchk(  ambiguous(SourceN),	Lsource),
+	memberchk(unambiguous(Target1), Ltarget),
+	memberchk(  ambiguous(TargetN),	Ltarget),
+
+	sort(Source1, S1), sort(SourceN, SN),
+	sort(Target1, T1), sort(TargetN, TN),
+
+	ord_intersection(S1, T1, Sel),
+	ord_union(SN, TN, Dis).
