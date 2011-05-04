b98b968c31878a3f34ddb2fda20dee7d06f773bb
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-05-04 16:16:28 +0200

    deprecate old ui stuff

--- config-available/ag_menu.pl
+++ config-available/ag_menu.pl
@@ -11,13 +11,15 @@ This module is a ClioPatria plugin that extends the ClioPatria menu.
 %
 %	Provide the Amalgame extensions to the Cliopatria menu.
 
+cliopatria:menu_popup_order(amalgame, 120).
+cliopatria:menu_item(100=amalgame/eq, 'Amalgame').
 
 cliopatria:menu_popup_order(vocabularies, 320).
-cliopatria:menu_item(100=vocabularies/http_concept_finder, 'Browse').
-cliopatria:menu_item(200=vocabularies/http_list_skos_vocs, 'Statistics').
+cliopatria:menu_item(100=vocabularies/http_concept_finder, 'Browse (deprecated)').
+cliopatria:menu_item(200=vocabularies/http_list_skos_vocs, 'Statistics (deprecated)').
 
 cliopatria:menu_popup_order(alignments, 350).
-cliopatria:menu_item(100=alignments/http_list_overlap,    'Overlaps').
-cliopatria:menu_item(200=alignments/http_list_alignments, 'Statistics').
+cliopatria:menu_item(100=alignments/http_list_overlap,    'Overlaps (deprecated)').
+cliopatria:menu_item(200=alignments/http_list_alignments, 'Statistics (deprecated)').
 
 
