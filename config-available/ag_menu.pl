3e88a5deda754fb5676f5d5f501807df366e2e67
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-06-03 08:43:31 +0200

    cleanup menu

--- config-available/ag_menu.pl
+++ config-available/ag_menu.pl
@@ -14,12 +14,12 @@ This module is a ClioPatria plugin that extends the ClioPatria menu.
 cliopatria:menu_popup_order(amalgame, 120).
 cliopatria:menu_item(100=amalgame/http_eq, 'Amalgame').
 
-cliopatria:menu_popup_order(vocabularies, 320).
-cliopatria:menu_item(100=vocabularies/http_concept_finder, 'Browse (deprecated)').
-cliopatria:menu_item(200=vocabularies/http_list_skos_vocs, 'Statistics (deprecated)').
+% cliopatria:menu_popup_order(vocabularies, 320).
+cliopatria:menu_item(250=amalgame/http_concept_finder, 'Browse (deprecated)').
+cliopatria:menu_item(260=amalgame/http_list_skos_vocs, 'Statistics (deprecated)').
 
-cliopatria:menu_popup_order(alignments, 350).
-cliopatria:menu_item(100=alignments/http_list_overlap,    'Overlaps (deprecated)').
-cliopatria:menu_item(200=alignments/http_list_alignments, 'Statistics (deprecated)').
+% cliopatria:menu_popup_order(alignments, 350).
+cliopatria:menu_item(300=amalgame/http_list_overlap,    'Overlaps (deprecated)').
+cliopatria:menu_item(350=amalgame/http_list_alignments, 'Statistics (deprecated)').
 
 
