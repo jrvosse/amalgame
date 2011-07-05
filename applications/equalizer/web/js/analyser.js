9571ff3c1fc073facb847a043905b985f3e65ae4
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-07-05 10:01:11 +0200

    add _onMappingSelect to analyser

--- applications/equalizer/web/js/analyser.js
+++ applications/equalizer/web/js/analyser.js
@@ -1,11 +1,11 @@
 YUI.add('analyser', function(Y) {
-	
+
 	var Lang = Y.Lang,
 		Node = Y.Node,
 		Plugin = Y.Plugin;
-	
-	var	NODE_MAPPINGS 			= Y.one("#mappings");
-	
+
+	var	NODE_MAPPINGS			= Y.one("#mappings");
+
 	function Analyser(config) {
 		Analyser.superclass.constructor.apply(this, arguments);
 	}
@@ -37,19 +37,28 @@ YUI.add('analyser', function(Y) {
 			}
 	    }
 	};
-	
+
 	Y.extend(Analyser, Y.Base, {
-		
+
 		initializer: function(args) {
 			this.mappinglist = new Y.MappingList({
 				mappings:this.get("mappings")
 			}).render(NODE_MAPPINGS);
-		}
-				
+			this.mappinglist.on("mappingSelect", this._onMappingSelect, this);
+		},
+
+		_onMappingSelect : function(e) {
+		       var uri = e.uri;
+		       this.set("selected", uri);
+		       this._fetchInfo(uri);
+		       this.mappingtable.set("mapping", uri);
+	         }
+
+
 	});
-	
+
 	Y.Analyser = Analyser;
-	
+
 }, '0.0.1', { requires: [
 	'node','event','anim',
 	'mappinglist'
