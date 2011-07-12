f701fb26ad6a41eb862c2c656f6ea24e544b89a9
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-07-12 23:06:43 +0200

    do not throw away original matching relation

--- lib/ag_modules/preloaded_mapping.pl
+++ lib/ag_modules/preloaded_mapping.pl
@@ -13,18 +13,16 @@ amalgame_module(amalgame:'SelectPreLoaded').
 parameter(name, 'atom', 'no default',
 	  'url of named mapping graph').
 
-%%	selecter(+Mapping, -Selected, -Discarded, -Undecided, +Options)
-%
-%	Selected contains only the unique correspondences between
-%	a source and target concept.
-
 matcher(Source, Target, Mapping, Options) :-
 	option(name(Graph), Options),
 	findall(align(S,T,P),
-		(   has_map([S,T],_,Graph),
+		(   has_map([S,T],_Format,Props,Graph),
 		    vocab_member(S, Source),
 		    vocab_member(T, Target),
-		    P=[preloaded(Graph)]
+		    (   option(relation(R), Props)
+		    ->	P = [[relation(R)]]
+		    ;	P = [[]]
+		    )
 		),
 		Mapping).
 
