9da5785c799514943828e6ab945acb3439b2a19a
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-08-02 16:25:53 +0200

    namespace of unsure and unrelated moved to evaluator:

--- applications/equalizer/mapping.pl
+++ applications/equalizer/mapping.pl
@@ -38,8 +38,8 @@ mapping_relation(close,     'http://www.w3.org/2004/02/skos/core#closeMatch').
 mapping_relation(narrower,  'http://www.w3.org/2004/02/skos/core#narrowMatch').
 mapping_relation(broader,   'http://www.w3.org/2004/02/skos/core#broadMatch').
 mapping_relation(related,   'http://www.w3.org/2004/02/skos/core#relatedMatch').
-mapping_relation(unrelated, 'http://purl.org/vocabularies/amalgame#unrelated').
-mapping_relation(unsure,    'http://purl.org/vocabularies/amalgame#unsure').
+mapping_relation(unrelated, 'http://purl.org/vocabularies/amalgame/evaluator#unrelated').
+mapping_relation(unsure,    'http://purl.org/vocabularies/amalgame/evaluator#unsure').
 
 %%	http_data_mapping(+Request)
 %
