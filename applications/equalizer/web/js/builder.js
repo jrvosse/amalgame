08323a3d1c02b7d0a1df9cfe876ff291c192e763
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-09-13 11:24:45 +0200

    disable controls on readonly

--- applications/equalizer/web/js/builder.js
+++ applications/equalizer/web/js/builder.js
@@ -1,14 +1,14 @@
 YUI.add('builder', function(Y) {
-	
+
 	var Lang = Y.Lang,
 		Node = Y.Node,
 		Plugin = Y.Plugin;
-	
-	var	NODE_OPM 			= Y.one("#opm"),
-		NODE_CONTROLS 		= Y.one("#controls"),
-		NODE_INFO	 		= Y.one("#info"),
-		NODE_SELECT 		= Y.one("#select");
-	
+
+	var	NODE_OPM			= Y.one("#opm"),
+		NODE_CONTROLS		= Y.one("#controls"),
+		NODE_INFO			= Y.one("#info"),
+		NODE_SELECT		= Y.one("#select");
+
 	function Builder(config) {
 		Builder.superclass.constructor.apply(this, arguments);
 	}
@@ -19,6 +19,9 @@ YUI.add('builder', function(Y) {
 		},
 		selected : {
 			value: null
+		},
+	        readonly : {
+			value: true
 		},
 		paths:{
 			value:{
@@ -46,21 +49,27 @@ YUI.add('builder', function(Y) {
 			}
 	    }
 	};
-	
+
 	Y.extend(Builder, Y.Base, {
-		
+
 		initializer: function(args) {
 			// initalize the different modules
 			this._initGraph();
 			this._initInfo();
-			this._initControls();			
-
+			this._initControls();
+			this.readonly = (this.get('readonly') != "false"); // string/boolean
+			if (!this.readonly) {
+				this.controls.on("submit", this._onControlSubmit, this);
+			        this.infobox.after("nodeChange", this._onNodeChange, this);
+			        this.infobox.after("deleteNode", this._onNodeDelete, this);
+                                Y.log('enabled  control submit handlers');
+			} else {
+                                Y.log('disabling  control submit handlers');
+			        Y.all('button').each(function(button) { button.setAttribute("disabled", true); });
+                        };
 			// bind the modules together
 			this.opmviz.on("nodeSelect", this._onNodeSelect, this);
-			this.controls.on("submit", this._onControlSubmit, this);
-			this.infobox.after("nodeChange", this._onNodeChange, this);
-			this.infobox.after("deleteNode", this._onNodeDelete, this);
-			
+
 			this.after('nodesChange', function(o) {
 				this.controls.set("nodes", o.newVal);
 			}, this);
@@ -71,7 +80,7 @@ YUI.add('builder', function(Y) {
 			this.infobox.set("selected", node);
 			this._fetchGraph();
 		},
-		
+
 		_initGraph : function() {
 			var DS = new Y.DataSource.IO({
 				source: this.get("paths").nodeinfo
@@ -84,7 +93,7 @@ YUI.add('builder', function(Y) {
 				alignment: alignment
 			}).render(NODE_OPM);
 		},
-		
+
 		_initInfo : function() {
 			// The infobox is part of the controls,
 			// but has some additional routines
@@ -98,25 +107,25 @@ YUI.add('builder', function(Y) {
 				datasource: DS
 			});
 		},
-		
-		_initControls : function() {			
+
+		_initControls : function() {
 			this.controls = new Y.Controls({
 				srcNode: NODE_CONTROLS,
 				selected: this.get("selected"),
 				nodes: this.get("nodes")
 			});
 		},
-		
+
 		_fetchGraph : function(conf) {
 			var alignment = this.get("alignment"),
 				paths = this.get("paths"),
 				opmviz = this.opmviz;
-				
+
 			if(alignment) {
 				conf = conf ? conf : {};
 				conf.graph = alignment;
-				
- 				Y.io(paths.opmgraph, {
+
+				Y.io(paths.opmgraph, {
 					data:conf,
 					on:{success: function(e,o) {
 						// As the server returns an XML document, including doctype
@@ -128,12 +137,12 @@ YUI.add('builder', function(Y) {
 				})
 			}
 		},
-		
+
 		_onControlSubmit : function(o) {
 			var oSelf = this,
 				paths = this.get("paths"),
 				data = o.data;
-			
+
 			// data only contains the process parameters
 			// we need to add the context
 			data.alignment = this.get("alignment");
@@ -145,13 +154,13 @@ YUI.add('builder', function(Y) {
 				}}
 			})
 		},
-		
+
 		_onNodeChange : function(o) {
 			var oSelf = this,
 				paths = this.get("paths"),
 				data = o.update;
 			data.alignment = this.get("alignment");
-			
+
 			Y.io(paths.updatenode, {
 				data:data,
 				on:{success:function(e,o) {
@@ -162,14 +171,14 @@ YUI.add('builder', function(Y) {
 					} else { // alignment changed name, we need to fully reload ...
 						oSelf.set("alignment", response.alignment);
 						var l = window.location;
-						var newURL = l.protocol + "//" + l.host + l.pathname + 
+						var newURL = l.protocol + "//" + l.host + l.pathname +
 							"?alignment=" + encodeURIComponent(response.alignment);
 						window.location.replace(newURL);
 					}
 				}}
 			})
 		},
-		
+
 		_onNodeDelete : function(o) {
 			var oSelf = this,
 				paths = this.get("paths"),
@@ -177,7 +186,7 @@ YUI.add('builder', function(Y) {
 					alignment:this.get("alignment"),
 					uri:o.uri
 				};
-			
+
 			Y.io(paths.deletenode, {
 				data:data,
 				on:{success:function(e,o) {
@@ -186,23 +195,23 @@ YUI.add('builder', function(Y) {
 				}}
 			})
 		},
-				
+
 		_onNodeSelect : function(e) {
 			var uri = e.uri,
 				node = this.get("nodes")[uri],
 				type = node.type,
 				label = node.label;
 			node.uri = uri;
-				
-			this.set("selected", uri);	
+
+			this.set("selected", uri);
 			// update the controls and the info
 			this.controls.set("selected", node);
 			this.infobox.set("selected", node);
 		}
 	});
-	
+
 	Y.Builder = Builder;
-	
+
 }, '0.0.1', { requires: [
 	'node','event','anim','tabview',
 	'datasource-io','datasource-cache',
