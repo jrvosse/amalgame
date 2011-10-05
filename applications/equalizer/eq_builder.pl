be9629eb43313824c5daadb9a1de4bb01ad24465
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2011-10-05 15:03:16 +0200

    experimenting with overlays, wip

--- applications/equalizer/eq_builder.pl
+++ applications/equalizer/eq_builder.pl
@@ -130,7 +130,7 @@ js_path(deletenode, Path) :-
 js_module(gallery, 'gallery-2011.02.23-19-01').
 js_module(builder, json([fullpath(Path),
 			   requires([node,event,
-				     'json-parse',
+				     'json-parse', 'overlay',
 				     'datasource-io','datasource-cache',
 				     'querystring-stringify-simple',
 				     opmviz,controls,infobox])
--- applications/equalizer/web/js/controls.js
+++ applications/equalizer/web/js/controls.js
@@ -1,9 +1,9 @@
 YUI.add('controls', function(Y) {
-	
+
 	var Lang = Y.Lang,
 		Node = Y.Node,
 		Widget = Y.Widget;
-	
+
 	var NODE_CONTROLS = Y.all(".control"),
 		NODE_INPUT_CONTROLS = Y.all("#select .control"),
 		NODE_INPUT = Y.one("#input"),
@@ -12,7 +12,7 @@ YUI.add('controls', function(Y) {
 		NODE_INPUT_BTN = Y.one("#inputbtn"),
 		NODE_SOURCE_BTN = Y.one("#sourcebtn"),
 		NODE_TARGET_BTN = Y.one("#targetbtn");
-	
+
 	function Controls(config) {
 		Controls.superclass.constructor.apply(this, arguments);
 	}
@@ -28,42 +28,45 @@ YUI.add('controls', function(Y) {
 			value: []
 		}
 	};
-	
+
 	Y.extend(Controls, Y.Base, {
 		initializer: function(config) {
 			var instance = this,
 				content = this.get("srcNode");
-			
+
 			// the display of the control sets can be toggled
 			Y.all(".control-set .hd").on("click", function(e) {
 				e.currentTarget.get("parentNode").toggleClass("active");
 			});
-			
+
 			// The list of amalgame modules make an accordion
-			Y.all(".module-list").plug(Y.Plugin.NodeAccordion, { 
+			Y.all(".module-list").plug(Y.Plugin.NodeAccordion, {
 				multiple:false
 			});
 
 			// The control all have submit button that we bind here
-			NODE_CONTROLS.each( function(node) {
-				node.one(".control-submit").on("click", this._onControlSubmit, this, node);
+			NODE_CONTROLS.each(function(node) {
+			   node.one(".control-submit").on("click", this._onControlSubmit, this, node);
+			   // var overlaynode = Node.create("<div>foobar</div>");
+			   // Y.log(overlaynode);
+			   // node.overlay = new Y.Overlay({ bodyContent: "<div>foobar</div>", visible: true, zIndex: width:"50%" }).render();
 			}, this);
-			
+
 			// the match control has two additional buttons
 			// to set the source and target
 			Y.on("click", this._valueSet, NODE_INPUT_BTN, this, "input");
 			Y.on("click", this._valueSet, NODE_SOURCE_BTN, this, "source");
-	      	Y.on("click", this._valueSet, NODE_TARGET_BTN, this, "target");
-			
+			Y.on("click", this._valueSet, NODE_TARGET_BTN, this, "target");
+
 			// secinput modules need additional control to allow the mappings for exclusion etc
 			this.after('nodesChange', this._setMappingSelecter, this);
 			this._setMappingSelecter();
-			
+
 			// toggle the controls when selected is changed
 			this.after('selectedChange', this._toggleControls, this);
 			this._toggleControls();
 		},
-		
+
 		_setMappingSelecter : function() {
 			var nodes = this.get("nodes");
 			Y.all(".secinput form").each( function(form) {
@@ -75,7 +78,7 @@ YUI.add('controls', function(Y) {
 				selecter.setContent(this.formatMappingList(nodes));
 			}, this);
 		},
-		
+
 		formatMappingList : function(nodes) {
 			var HTML = "";
 			for (var uri in nodes) {
@@ -90,17 +93,17 @@ YUI.add('controls', function(Y) {
 			}
 			return HTML;
 		},
-				
+
 		_onControlSubmit : function(e, node) {
 			e.preventDefault();
-			
+
 			var content = this.get("srcNode"),
 				input = NODE_INPUT.get("value"),
 				source = NODE_SOURCE.get("value"),
 				target = NODE_TARGET.get("value"),
 				selected = this.get("selected"),
 				data = this._getFormData(node);
- 
+
 			// The input is selected based on the type of the control
 			// which is stored as a CSS class
 			if(node.hasClass("match")) {
@@ -115,11 +118,11 @@ YUI.add('controls', function(Y) {
 			else if(selected) {
 				data.input = selected.uri;
 			}
-			
+
 			this.fire("submit", {data:data});
 
 		},
-		
+
 		_getFormData : function(form) {
 			var data = {};
 			// get the values of all HTML input fields
@@ -148,10 +151,10 @@ YUI.add('controls', function(Y) {
 					data[name] = value;
 				}
 			});
-			
+
 			return data;
 		},
-		
+
 		_toggleControls : function() {
 			var selected = this.get("selected"),
 				type = selected ? selected.type : "";
@@ -163,7 +166,7 @@ YUI.add('controls', function(Y) {
 					node.addClass("disabled");
 				}
 			});
-			
+
 			// enable input select when a vocabulary is selected
 			NODE_INPUT_BTN.setAttribute("disabled", true);
 			NODE_SOURCE_BTN.setAttribute("disabled", true);
@@ -175,7 +178,8 @@ YUI.add('controls', function(Y) {
 				NODE_INPUT_BTN.removeAttribute("disabled");
 			}
 			var nodes = this.get("nodes");
-			if (!nodes.value) {
+			if (nodes.length < 1) {
+			        Y.log("Disabling components requiring secondary inputs");
 				Y.log(nodes);
 				Y.all(".secinput").addClass("disabled");
 			}
@@ -187,14 +191,14 @@ YUI.add('controls', function(Y) {
 				Y.all("#match .control-submit").setAttribute("disabled", true);
 			}
 		},
-		
+
 		_valueSet : function(e, which) {
 			var selected =  this.get("selected");
 			if(selected) {
 				Y.one("#"+which+'Label').set("value", selected.label);
 				Y.one("#"+which).set("value", selected.uri);
 				this._toggleControls();
-     		}
+		}
 			if(which=="input") {
 				Y.one("#sourceLabel").set("value", "");
 				Y.one("#source").set("value", "");
@@ -205,9 +209,16 @@ YUI.add('controls', function(Y) {
 				Y.one("#input").set("value", "");
 			}
 		}
-		
+
 	});
-	
+
 	Y.Controls = Controls;
-	
-}, '0.0.1', { requires: ['node,event','anim','gallery-node-accordion']});
+
+}, '0.0.1', {
+	    requires: ['node',
+		       'event',
+		       'overlay',
+		       'anim',
+		       'gallery-node-accordion'
+		      ]
+	    });
