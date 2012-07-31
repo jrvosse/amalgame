e735232a3e0a93413d55f034ea36e05609b0c3aa
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2012-07-31 15:33:52 +0200

    Fixed \c issue

--- applications/evaluator/evaluator.pl
+++ applications/evaluator/evaluator.pl
@@ -31,7 +31,7 @@
 :- use_module(library(json_graph)).
 
 :- setting(evaluator:maxMappings, nonneg, 1000,
-	   'Max number of mappings per mapping file to put in the \
+	   'Max number of mappings per mapping file to put in the \c
 	   todo list of the mapping evaluator').
 :- setting(evaluator:authorization, oneof([required, optional]), required,
 	   'Login required to evaluate or not').
