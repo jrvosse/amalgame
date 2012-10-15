6137ed7cf89880512df83e6caa08fbfa8cd227d1
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2012-10-15 15:28:24 +0200

    adapt to new stricter turtle parser

--- config-available/020-ag_prefixes.pl
+++ config-available/020-ag_prefixes.pl
@@ -7,13 +7,13 @@
 % Prolog or ClioPatria.
 
 % Namespaces from third parties we use/reuse:
-:- rdf_register_ns(align, 'http://knowledgeweb.semanticweb.org/heterogeneity/alignment#').
+% :- rdf_register_ns(align, 'http://knowledgeweb.semanticweb.org/heterogeneity/alignment#').
+% :- rdf_register_ns(void, 'http://rdfs.org/ns/void#').
 :- rdf_register_ns(time, 'http://www.w3.org/2006/time#').
-:- rdf_register_ns(void, 'http://rdfs.org/ns/void#').
 :- rdf_register_ns(edm,	 'http://www.europeana.eu/schemas/edm/').
 
 % Namespaces we declared ourselves:
-:- rdf_register_ns(amalgame,   'http://purl.org/vocabularies/amalgame#').
+% :- rdf_register_ns(amalgame,   'http://purl.org/vocabularies/amalgame#').
 :- rdf_register_ns(evaluator,  'http://purl.org/vocabularies/amalgame/evaluator#').
 
 % Namespaces that are not used in the code but have no obvious location
