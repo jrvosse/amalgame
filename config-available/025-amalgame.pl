b019ea2fb3cb3594dcbf263cf6dcf9606ce329e9
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-01-28 16:59:14 +0100

    merged config loader and equalizer loader into one

--- config-available/025-amalgame.pl
+++ config-available/025-amalgame.pl
@@ -1,27 +1,65 @@
 :- module(conf_amalgame, []).
-:- use_module(library(semweb/rdf_library)).
-:- use_module(library(skos_schema)).
-:- use_module(library(void_schema)).
 
 /** <module> Setup amalgame
 */
 
-:- multifile http:location/3.
+:- use_module(library(semweb/rdf_library)).
+:- use_module(library(http/http_dispatch)).
+:- use_module(library(http/http_dirindex)).
 
-http:location(amalgame, cliopatria(amalgame), []).
+:- use_module(library(skos_schema)).
+:- use_module(library(void_schema)).
+:- use_module(library(prov_schema)).
+
+:- multifile
+	http:location/3.		% Alias, Expansion, Options
+:- dynamic
+	http:location/3.		% Alias, Expansion, Options
+
+http:location(amalgame,          cliopatria(amalgame),       []).
+http:location(alignment_results, root(alignment_results),    [ priority(-100) ]).
+http:location(img,		 root(img),                  [ priority(-100) ]).
 
 :- rdf_attach_library(amalgame(rdf)).
 :- rdf_load_library(amalgame).
 :- rdf_load_library(dc).
 
-:- use_module([
-	% Deprecated:
-	%	applications(align_stats),
-	%	applications(alignment/alignment),
-	%	applications(vocabularies/vocabularies),
-	%	applications(evaluator/evaluator),
-	% New UI:
-		applications(equalizer/eq),
-	% HTTP API to export mappings to other web services:
-		api(http_mapping)
-	      ]).
+/* Now all namespaces should have been defined and we can load the amalgame code: */
+:- use_module(applications(equalizer/eq_selecter)).
+:- use_module(applications(equalizer/eq_builder)).
+:- use_module(applications(equalizer/eq_analyser)).
+:- use_module(applications(equalizer/eq_evaluater)).
+:- use_module(applications(equalizer/eq_publisher)).
+:- use_module(api(http_mapping)).
+
+% add local web directories from which static files are served.
+
+:- prolog_load_context(directory, Dir),
+   asserta(user:file_search_path(eq, Dir)).
+:- asserta(user:file_search_path(css, eq(web/css))).
+:- asserta(user:file_search_path(js, eq(web/js))).
+:- asserta(user:file_search_path(icon, eq(web/icon))).
+:- asserta(user:file_search_path(alignment_results, web(alignment_results))).
+:- asserta(user:file_search_path(img, web(img))).
+
+:- http_handler(alignment_results(.), serve_static(alignment_results), [prefix]).
+:- http_handler(img(.),               serve_static(img),               [prefix]).
+
+serve_static(Alias, Request) :-
+	memberchk(path(PathInfo), Request),
+	sub_atom(PathInfo, 1, Len, End, Alias),
+	(   End < 2
+	->  Path='.'
+	;   debug(foo, '~w', [End]),
+	    Start is Len + 2,
+	    sub_atom(PathInfo, Start, _, 0, Path)
+	),
+	Term =.. [Alias,Path],
+	(   absolute_file_name(Term, _,
+			       [file_type(directory),
+				access(read),
+				file_errors(fail)
+			       ])
+	->  http_reply_dirindex(Term, [unsafe(true)], Request)
+	;   http_reply_file(Term, [], Request)
+	).
