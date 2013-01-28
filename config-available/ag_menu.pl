b7a0b80313482ea6a6522a1ca447c62bebad3450
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-01-28 16:44:19 +0100

    remove old apps from menu

--- config-available/ag_menu.pl
+++ config-available/ag_menu.pl
@@ -13,14 +13,5 @@ This module is a ClioPatria plugin that extends the ClioPatria menu.
 %	Provide the Amalgame extensions to the Cliopatria menu.
 
 cliopatria:menu_popup_order(amalgame, 120).
-cliopatria:menu_item(100=amalgame/http_eq, 'Amalgame (new, experimental UI)').
-
-% cliopatria:menu_popup_order(vocabularies, 320).
-cliopatria:menu_item(250=amalgame/http_skos_browser,   'Voc browser (old UI)').
-cliopatria:menu_item(260=amalgame/http_list_skos_vocs, 'Voc stats   (old UI)').
-
-% cliopatria:menu_popup_order(alignments, 350).
-cliopatria:menu_item(300=amalgame/http_list_alignments,'Map stats   (old UI)').
-cliopatria:menu_item(350=amalgame/http_list_overlap,   'Map overlap (old UI)').
-
-
+cliopatria:menu_item(100=amalgame/http_eq, 'Alignment interface').
+cliopatria:menu_item(250=amalgame/http_skos_browser,   'Vocabulary browser').
