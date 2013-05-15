cbabbb3df2d0b06a79dfbc604009b6b344c82c97
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-05-15 09:55:08 +0200

    minor

--- lib/skos/vocabularies.pl
+++ lib/skos/vocabularies.pl
@@ -9,7 +9,6 @@
 
 :- use_module(library(semweb/rdfs)).
 :- use_module(library(semweb/rdf_db)).
-:- use_module(library(semweb/rdf_label)).
 
 :- use_module(library(amalgame/map)).
 :- use_module(library(amalgame/ag_provenance)).
