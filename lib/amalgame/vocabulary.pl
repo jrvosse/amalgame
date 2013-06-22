369a28eeccc6c56ca2f23c9660ac0589fc2e41a7
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-06-22 20:34:34 +0200

    ADDED: not(Def) to vocab_member/2

--- lib/amalgame/vocabulary.pl
+++ lib/amalgame/vocabulary.pl
@@ -12,6 +12,10 @@
 %	@param VocabDef is a URI of a skos:ConceptScheme or a definition
 %	of a subset thereof.
 
+vocab_member(C, not(Def)) :-
+	ground(C),
+	ground(Def),
+	\+ vocab_member(C, Def).
 vocab_member(E, and(G1,G2)) :-
 	!,
 	vocab_member(E,G1),
@@ -22,13 +26,12 @@ vocab_member(E, scheme(Scheme)) :-
 vocab_member(E, type(Class)) :-
 	!,
 	rdfs_individual_of(E, Class).
-
 vocab_member(E, graph(G)) :-
+	!,
 	rdf(E, rdf:type, _, G).
-
 vocab_member(E, propvalue(Property, Value)) :-
+	!,
 	rdf(E, Property, Value).
-
 vocab_member(F, 'http://sws.geonames.org/') :-
 	!,
 	rdfs_individual_of(F, 'http://www.geonames.org/ontology#Feature').
@@ -37,7 +40,6 @@ vocab_member(E, Scheme) :-
 	rdfs_individual_of(Scheme, skos:'ConceptScheme'),
 	!,
 	rdf_has(E, skos:inScheme, Scheme).
-
 vocab_member(E, Alignable) :-
 	atom(Alignable),
 	rdfs_individual_of(Alignable, amalgame:'Alignable'),
@@ -46,7 +48,6 @@ vocab_member(E, Alignable) :-
 	!,
 	vocab_member(E, graph(Graph)),
 	vocab_member(E, type(Class)).
-
 vocab_member(I, EDMGraph) :-
 	atom(EDMGraph),
 	rdf(_, 'http://www.europeana.eu/schemas/edm/country',_, EDMGraph),
@@ -57,5 +58,3 @@ vocab_member(E, Class) :-
 	rdfs_individual_of(Class, rdfs:'Class'),
 	!,
 	rdfs_individual_of(E, Class).
-
-
