b84486b96771f8462a0934dd24926f8fc71c6917
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-08-23 12:22:55 +0200

    also clear nickname_cache on make - just to be sure

--- lib/amalgame/caching.pl
+++ lib/amalgame/caching.pl
@@ -30,6 +30,7 @@
 user:message_hook(make(done(_)), _, _) :-
 	debug(ag_expand, 'Flushing mapping statistics cache after running make/0', []),
 	flush_stats_cache(_),
+	nickname_clear_cache,
 	fail.
 user:message_hook(make(done(_)), _, _) :-
 	debug(ag_expand, 'Flushing expand mapping cache after running make/0', []),
