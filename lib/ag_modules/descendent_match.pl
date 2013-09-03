11a8cccc2528e384f97d0c0d345f57a461d9f952
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-09-03 18:26:03 +0200

    select all matches, not only the best

--- lib/ag_modules/descendent_match.pl
+++ lib/ag_modules/descendent_match.pl
@@ -4,7 +4,7 @@
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(amalgame/vocabulary)).
 :- use_module(library(amalgame/map)).
-:- use_module(library(count)).
+:- use_module(string_match_util).
 
 :- public amalgame_module/1.
 :- public filter/3.
@@ -17,6 +17,9 @@ amalgame_module(amalgame:'DescendentFilter').
 amalgame_module(amalgame:'DescendentSelecter').
 
 parameter(steps, integer, 1, 'depth of search, defaults to 1, e.g. direct children only').
+parameter(type,
+          oneof([source, target]), source,
+          'Discard other matches with the same source/target').
 
 %%	filter(+MappingsIn, -MappingsOut, +Options)
 %
@@ -93,7 +96,7 @@ selecter(In, Sel, Dis, Und, Options) :-
 	findall(S-T-P, member(align(S,T,P), SecList), KeyValueList),
 	keysort(KeyValueList, Deduped),
 	ord_list_to_assoc(Deduped, BackgroundMatches),
-	(   SourceOrTarget = target
+	(   SourceOrTarget = source
 	->  selecter_(SourceOrTarget, In, BackgroundMatches, Sel, Dis, Und, Options)
 	;   predsort(ag_map:compare_align(target), In, InT),
 	    selecter_(SourceOrTarget, InT, BackgroundMatches, Sel0, Dis0, Und0, Options),
@@ -105,17 +108,19 @@ selecter(In, Sel, Dis, Und, Options) :-
 selecter_(_, [], _, [], [], [], _).
 selecter_(Type, [Head|Tail], BackgroundMatches, Sel, Dis, Und, Options) :-
 	Head = align(S,T,_),
-	(   Type == target
+	(   Type == source
 	->  same_source(Tail, S, Same, Rest)
 	;   same_target(Tail, T, Same, Rest)
 	),
 	selecter_(Type, Rest, BackgroundMatches, TailSel, TailDis, TailUnd, Options),
 	Candidates = [Head|Same],
-	maplist(descendent_count(BackgroundMatches, Options), Candidates, Counts),
-	keysort(Counts, [BestCount-BestCorr|DisgardedSame0]),
-	(   Same \= [], BestCount < 1
-	->  append([[BestCorr],    TailSel], Sel),
-	    pairs_values(DisgardedSame0, DisgardedSame),
+	maplist(descendent_count(BackgroundMatches, Options), Candidates, Counts0),
+	keysort(Counts0, Counts),
+	zero_keys(Counts, Zero, NonZero),
+	(   NonZero \= []
+	->  pairs_values(Zero,   DisgardedSame),
+	    pairs_values(NonZero, SelectedSame),
+	    append([SelectedSame,  TailSel], Sel),
 	    append([DisgardedSame, TailDis], Dis),
 	    Und = TailUnd
 	;   Sel = TailSel,
@@ -123,9 +128,12 @@ selecter_(Type, [Head|Tail], BackgroundMatches, Sel, Dis, Und, Options) :-
 	    append([Candidates, TailUnd], Und)
 	).
 
-descendent_count(BackgroundMatches, Options, Corr, Count-Corr) :-
-	answer_count(Pnew,
-		     match(Corr, BackgroundMatches, align(_S,_T,Pnew), Options),
-		     10,
-		     Count0),
-	Count is 1/(1+Count0).
+descendent_count(BackgroundMatches, Options, Corr, Count-Merged) :-
+	findall(Match,
+		match(Corr, BackgroundMatches, Match, Options),
+		Matches),
+	length(Matches, Count),
+	(   Count > 0
+	->  merge_provenance(Matches, [Merged])
+	;   Merged = Corr
+	).
