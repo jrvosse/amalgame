cc79a038982e8a07877d390a5dc00a062d35f9af
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-09-03 15:27:25 +0200

    added selecter mode for descendent matcher

--- lib/ag_modules/descendent_match.pl
+++ lib/ag_modules/descendent_match.pl
@@ -3,14 +3,18 @@
 
 :- use_module(library(semweb/rdf_db)).
 :- use_module(library(amalgame/vocabulary)).
+:- use_module(library(amalgame/map)).
+:- use_module(library(count)).
 
 :- public amalgame_module/1.
 :- public filter/3.
 :- public matcher/4.
+:- public selecter/5.
 :- public parameter/4.
 
 amalgame_module(amalgame:'DescendentMatcher').
 amalgame_module(amalgame:'DescendentFilter').
+amalgame_module(amalgame:'DescendentSelecter').
 
 parameter(steps, integer, 1, 'depth of search, defaults to 1, e.g. direct children only').
 
@@ -82,3 +86,46 @@ descendent(R, MaxSteps, Child, rdf(Child, Prop, R), Steps) :-
 	rdf_reachable(Child, Prop, R, MaxSteps, Steps),
 	\+ R == Child,
 	\+ rdf_reachable(R, skos:narrower, Child).
+
+selecter(In, Sel, Dis, Und, Options) :-
+	option(snd_input(SecList), Options),
+	option(type(SourceOrTarget), Options, source),
+	findall(S-T-P, member(align(S,T,P), SecList), KeyValueList),
+	keysort(KeyValueList, Deduped),
+	ord_list_to_assoc(Deduped, BackgroundMatches),
+	(   SourceOrTarget = target
+	->  selecter_(SourceOrTarget, In, BackgroundMatches, Sel, Dis, Und, Options)
+	;   predsort(ag_map:compare_align(target), In, InT),
+	    selecter_(SourceOrTarget, InT, BackgroundMatches, Sel0, Dis0, Und0, Options),
+	    predsort(ag_map:compare_align(source), Sel0,  Sel),
+	    predsort(ag_map:compare_align(source), Dis0,  Dis),
+	    predsort(ag_map:compare_align(source), Und0,  Und)
+	).
+
+selecter_(_, [], _, [], [], [], _).
+selecter_(Type, [Head|Tail], BackgroundMatches, Sel, Dis, Und, Options) :-
+	Head = align(S,T,_),
+	(   Type == target
+	->  same_source(Tail, S, Same, Rest)
+	;   same_target(Tail, T, Same, Rest)
+	),
+	selecter_(Type, Rest, BackgroundMatches, TailSel, TailDis, TailUnd, Options),
+	Candidates = [Head|Same],
+	maplist(descendent_count(BackgroundMatches, Options), Candidates, Counts),
+	keysort(Counts, [BestCount-BestCorr|DisgardedSame0]),
+	(   Same \= [], BestCount < 1
+	->  append([[BestCorr],    TailSel], Sel),
+	    pairs_values(DisgardedSame0, DisgardedSame),
+	    append([DisgardedSame, TailDis], Dis),
+	    Und = TailUnd
+	;   Sel = TailSel,
+	    Dis = TailDis,
+	    append([Candidates, TailUnd], Und)
+	).
+
+descendent_count(BackgroundMatches, Options, Corr, Count-Corr) :-
+	answer_count(Pnew,
+		     match(Corr, BackgroundMatches, align(_S,_T,Pnew), Options),
+		     10,
+		     Count0),
+	Count is 1/(1+Count0).
