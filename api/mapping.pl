2dc918f8fc2b77b6b93a10db8e492b65b3bfae63
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2013-09-03 16:44:02 +0200

    hack: prefer direct over inherited skos:notation

--- api/mapping.pl
+++ api/mapping.pl
@@ -73,13 +73,13 @@ mapping_label(align(S, T, Prov), align(S,SLabel, T,TLabel, Relation)) :-
 	rdf_display_label(S, SL),
 	rdf_display_label(T, TL),
 
-	(   rdf_has(S, skos:notation, SN)
+	(   (rdf(S, skos:notation, SN) ; rdf_has(S, skos:notation, SN))
 	->  literal_text(SN, Sn),
 	    format(atom(SLabel), '~w (~w)', [SL, Sn])
 	;   format(atom(SLabel), '~w', [SL])
 	),
 
-	(   rdf_has(T, skos:notation, TN)
+	(   (rdf(T, skos:notation, TN) ; rdf_has(T, skos:notation, TN))
 	->  literal_text(TN, Tn),
 	    format(atom(TLabel), '~w (~w)', [TL, Tn])
 	;   format(atom(TLabel), '~w', [TL])
