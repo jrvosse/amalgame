ab6941e5463fe73c5d8c6b84a01b2120f763fd02
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2014-08-28 10:46:49 +0200

    ADDED: select/remove mode for type selecter

--- lib/ag_modules/type_select.pl
+++ lib/ag_modules/type_select.pl
@@ -8,9 +8,15 @@ amalgame_module(amalgame:'TypeSelect').
 
 parameter(class, uri, '',
 	  'rdfs:Class from which to select the concepts').
+parameter(mode, oneof([select, remove]), select,
+	  'select or remove concepts of this type').
 
 % a bit naive at the moment: we simply change the query that should be
 % done.
 
 selecter(Scheme, and((Scheme), type(Class)), Options) :-
+	option(mode(select), Options),
+	option(class(Class), Options).
+selecter(Scheme, and((Scheme), not(type(Class))), Options) :-
+	option(mode(remove), Options),
 	option(class(Class), Options).
