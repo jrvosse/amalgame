134f9f6e406e617b6e8bfcfddc9eec98e53f93c8
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2014-08-30 15:35:17 +0200

    IMPROVED: related evidence more similar to ancestor match

--- lib/ag_modules/related.pl
+++ lib/ag_modules/related.pl
@@ -16,6 +16,6 @@ related_match(align(S, T, Prov0), BackgroundMatches, align(S, T, [Prov|Prov0]), 
 		graph([R1,R2])
 	       ].
 
-related(R, MaxSteps, Related, rdf_reachable(R, Prop, Related), Steps) :-
+related(R, MaxSteps, Related, rdf(R, Prop, Related), Steps) :-
 	skos_related_to(R, Related, MaxSteps, Steps),
-	rdf_equal(skos:related, Prop).
+	rdf_equal(amalgame:relatedTransitive, Prop).
