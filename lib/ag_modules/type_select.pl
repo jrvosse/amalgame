f6c0d84fbc1625abb9b7c6db58267e690534ef7b
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2014-09-02 17:29:59 +0200

    ADDED: TypePartition as our first concrete partition/5 module

--- lib/ag_modules/type_select.pl
+++ lib/ag_modules/type_select.pl
@@ -3,16 +3,15 @@
 :- public amalgame_module/1.
 :- public parameter/4.
 :- public selecter/3.
+:- public partition/5.
 
 amalgame_module(amalgame:'TypeSelect').
+amalgame_module(amalgame:'TypePartition').
 
 parameter(class, uri, '',
 	  'rdfs:Class from which to select the concepts').
 parameter(mode, oneof([select, remove]), select,
 	  'select or remove concepts of this type').
-
-% a bit naive at the moment: we simply change the query that should be
-% done.
 
 selecter(Scheme, and((Scheme), type(Class)), Options) :-
 	option(mode(select), Options),
@@ -20,3 +19,10 @@ selecter(Scheme, and((Scheme), type(Class)), Options) :-
 selecter(Scheme, and((Scheme), not(type(Class))), Options) :-
 	option(mode(remove), Options),
 	option(class(Class), Options).
+
+partition(VocSpec, Sel, Dis, [], Options) :-
+	option(mode(select), Options),
+	option(class(Class), Options),
+	Sel = and((VocSpec), type(Class)),
+	Dis = and((VocSpec), not(type(Class))).
+
