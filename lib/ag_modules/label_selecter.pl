07e760bdbb3de93e5360c9a98ce5b02faee7dcec
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2014-09-08 14:22:59 +0200

    ADDED: best source/target selecter mode for label-type selecters

--- lib/ag_modules/label_selecter.pl
+++ lib/ag_modules/label_selecter.pl
@@ -6,6 +6,13 @@
  * label matching partitioners.
  */
 
+:- use_module(library(apply)).
+:- use_module(library(lists)).
+:- use_module(library(option)).
+:- use_module(library(pairs)).
+:- use_module(library(sort)).
+:- use_module(library(amalgame/map)).
+
 :- meta_predicate label_selecter(3, +, -, -, -, +).
 
 label_selecter(Matcher, In, Sel, Dis, Und, Options) :-
@@ -21,6 +28,7 @@ label_selecter(Matcher, In, Sel, Dis, Und, Options) :-
 
 label_selecter(  _, _, [],  [],  [],  [], _).
 label_selecter(all, Matcher, [Head|Tail], Sel, Dis, [], Options) :-
+	!,
 	(   call(Matcher, Head, Match, Options)
 	->  Sel = [Match|TSel],
 	    Dis = TDis
@@ -31,3 +39,41 @@ label_selecter(all, Matcher, [Head|Tail], Sel, Dis, [], Options) :-
 	    Dis = [MisMatch|TDis]
 	),
 	label_selecter(all, Matcher, Tail, TSel, TDis, [], Options).
+
+label_selecter(Type, Matcher, [Head|Tail], Sel, Dis, Und, Options) :-
+	Head = align(S,T,_),
+	(   Type == target
+	->  same_source(Tail, S, Same, Rest)
+	;   same_target(Tail, T, Same, Rest)
+	),
+	% Fix me: make this tail recursive
+	label_selecter(Type, Matcher, Rest, TailSel, TailDis, TailUnd, Options),
+
+	Candidates = [Head|Same],
+	(   pick_best(Candidates, Matcher, SelectedSame, DisgardedSame, Options)
+	->  append([SelectedSame,  TailSel], Sel),
+	    append([DisgardedSame, TailDis], Dis),
+	    Und = TailUnd
+	;   Sel = TailSel,
+	    Dis = TailDis,
+	    append([Candidates, TailUnd], Und)
+	).
+
+pick_best(Candidates, Matcher, [Selected], Disgarded, Options) :-
+	length(Candidates, N), N > 1, % do not pick a best if we have only one alternative
+	maplist(label_count(Matcher, Options), Candidates, Counts0),
+	keysort(Counts0, Sorted),
+	append([DiscardedPairs, [SecondBest-Value2], [Best-Selected]], Sorted),
+	Best > SecondBest,
+	Best > 0,
+	pairs_values([SecondBest-Value2|DiscardedPairs], Disgarded).
+
+label_count(Matcher, Options, Corr, Count-Merged) :-
+	findall(Match,
+		call(Matcher, Corr, Match, Options),
+		Matches),
+	length(Matches, Count),
+	(   Count > 0
+	->  merge_provenance(Matches, [Merged])
+	;   Merged = Corr
+	).
