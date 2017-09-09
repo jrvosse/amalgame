b1fa82c0a8b4fc2f53cff144a71587a544fb068a
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2017-09-09 11:51:47 +0200

    simplify things by reusing stuff from cliopatria image

--- docker/run.sh
+++ docker/run.sh
@@ -4,11 +4,10 @@ APP=amalgame
 REPO=jrvosse
 HPORT=3020 # Port on host computer
 CPORT=3020 # Port on docker container running ClioPatria
-RUN=${1-./run.pl}
 sudo docker run \
 	-v $PWD:/opt/project \
 	-v /etc/group:/etc/group:ro \
 	-v /etc/passwd:/etc/passwd:ro \
 	-u $( id -u $USER ):$( id -g $USER ) \
 	-p $HPORT:$CPORT -it  \
-	$REPO/$APP:$VERSION $RUN
+	$REPO/$APP:$VERSION $@
