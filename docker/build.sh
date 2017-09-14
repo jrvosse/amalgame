84f7278205b1ed1d4cb2958d1ea1a19eed47187d
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2017-09-14 13:17:43 +0200

    minor

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,5 @@
 VERSION=latest
 APP=amalgame
 REPO=jrvosse
-sudo docker rmi $REPO/$APP:$VERSION
-sudo docker build --tag $REPO/$APP:$VERSION  .
+SUDO=
+$SUDO docker build --tag $REPO/$APP:$VERSION  .
