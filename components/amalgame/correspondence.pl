def4e53f487b9ed9bfe10268a867dad4b2567496
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2020-02-28 10:02:15 +0100

    correspondence.pl -> rdf11

--- components/amalgame/correspondence.pl
+++ components/amalgame/correspondence.pl
@@ -7,7 +7,7 @@
 :- use_module(library(http/html_write)).
 :- use_module(library(http/http_dispatch)).
 
-:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf11)).
 :- use_module(library(semweb/rdf_label)).
 :- use_module(components(label)).
 :- use_module(components(graphviz)).
