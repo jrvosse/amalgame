2e7ec0476a96643370ed4fa75e4b281e618fb758
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2020-03-02 11:29:09 +0100

    FIXED: many bugs in totally broken isub matcher ...

--- lib/ag_modules/isub_match.pl
+++ lib/ag_modules/isub_match.pl
@@ -2,15 +2,17 @@
 	  [isub_match/3]).
 
 :- use_module(library(option)).
-:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf11)).
 :- use_module(library(semweb/rdf_label)).
 :- use_module(library(isub)).
+:- use_module(string_match_util).
+
 
 isub_match(align(Source, Target, Prov0), align(Source, Target, [Prov|Prov0]), Options) :-
 	rdf_equal(skos:definition, DefaultProp),
 	option(threshold(Threshold), Options, 0.0),
-	option(sourcelabel(MatchProp1), Options, DefaultProp),
-	option(targetlabel(MatchProp2), Options, DefaultProp),
+	option(sourcelabel(MatchPropS), Options, DefaultProp),
+	option(targetlabel(MatchPropT), Options, DefaultProp),
 	option(matchacross_lang(MatchAcross), Options, true),
 	option(normalize(Normalize), Options, false),
 	option(source_language(Lang), Options, 'any'),
@@ -26,16 +28,13 @@ isub_match(align(Source, Target, Prov0), align(Source, Target, [Prov|Prov0]), Op
 	;   true
 	),
 
-	SearchTarget=literal(lang(TargetLang, TargetLit)),
 
-	(   rdf_has(Source, MatchProp1,  literal(lang(SourceLang, SourceLit)), SourceProp),
-	    rdf_has(Target, MatchProp2, SearchTarget, TargetProp),
-	    Source \== Target
-	->  literal_text(SourceLit, SourceTxt),
-	    literal_text(TargetLit, TargetTxt),
-	    isub(SourceTxt, TargetTxt, Normalize, Similarity)
-	;   Similarity = 0
-	),
+	skos_has(Source, MatchPropS, SourceLit@SourceLang, SourceProp, Options),
+	skos_has(Target, MatchPropT, TargetLit@TargetLang, TargetProp, Options),
+	Source \== Target,
+	literal_text(SourceLit, SourceTxt),
+	literal_text(TargetLit, TargetTxt),
+	isub(SourceTxt, TargetTxt, Normalize, Similarity),
 	Similarity > Threshold,
 	Prov = [method(isub),
 		match(Similarity),
