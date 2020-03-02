7b15e92e82789ded85877891b79233f65e42a65f
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2020-03-02 15:32:21 +0100

    rdf11

--- lib/ag_modules/snowball_label_selecter.pl
+++ lib/ag_modules/snowball_label_selecter.pl
@@ -2,7 +2,7 @@
 	  []).
 
 :- use_module(library(amalgame/vocabulary)).
-:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf11)).
 :- use_module(library(snowball)).
 :- use_module(string_match_util).
 :- use_module(snowball_label_match).
