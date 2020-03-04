ad56200dbdcc5c0436f4ba5f938b54682788ed11
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2020-03-04 16:45:08 +0100

    rdf11

--- lib/amalgame/vocabulary.pl
+++ lib/amalgame/vocabulary.pl
@@ -14,7 +14,7 @@
 :- use_module(library(ordsets)).
 :- use_module(library(option)).
 :- use_module(library(settings)).
-:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf11)).
 :- use_module(library(semweb/rdfs)).
 :- use_module(library(skos/util)).
 :- use_module(ag_stats).
