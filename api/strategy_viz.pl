0d47f57af3cd76e922195c50520ac42293f05dbe
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2020-03-29 16:46:28 +0200

    rdf11. Removed final explict import of rdf_db

--- api/strategy_viz.pl
+++ api/strategy_viz.pl
@@ -5,7 +5,7 @@
 :- use_module(library(http/http_parameters)).
 :- use_module(library(http/html_write)).
 
-:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdf11)).
 :- use_module(library(semweb/rdfs)).
 :- use_module(library(semweb/rdf_label)).
 :- use_module(components(label)).
